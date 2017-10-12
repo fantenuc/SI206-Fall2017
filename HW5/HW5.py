2d95358e03105d16c46e529363e7e24af20876c4
Frankie Antenucci <fantenuc@umich.edu>
2017-10-11 22:09:07 -0400

    changed repositories

--- HW5/HW5.py
+++
@@ -1,8 +0,0 @@
-import re 
-
-dataFile = open('actualdata.txt')
-dataFile = dataFile.read()
-numbers = re.findall('[0-9]+', dataFile)
-# print(numbers)
-lst = sum([int(num) for num in numbers])
-print(lst)
